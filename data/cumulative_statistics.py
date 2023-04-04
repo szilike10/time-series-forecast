ca5a51fd114e50c4ceb8770f399ef739ff7a3e63
Balog Szilard (BEG/ENS1-RO) <Szilard.Balog@ro.bosch.com>
2023-04-04 13:17:31 +0300

    Changing cumulated dataframes to have date indexes for the forecasting frameworks.

--- data/cumulative_statistics.py
+++ data/cumulative_statistics.py
@@ -10,15 +10,17 @@ class CumStat:
     A class to quickly apply aggregations on the given dataframe.
     """
 
-    def __init__(self, path_to_csv=None, df=None):
-        if path_to_csv and df:
-            raise Exception('CumStat object should initialized with either a path to a CSV file or a DataFrame.')
+    def __init__(self, df=None, path_to_csv=None, ):
+        if (path_to_csv and df) or (path_to_csv is None and df is None):
+            raise Exception('CumStat object should be initialized with either a path to a CSV file or a DataFrame.')
         self.path_to_csv = None
         if path_to_csv:
             self.path_to_csv = path_to_csv
             self.df = pd.read_csv(path_to_csv)
+
         else:
             self.df = df
+        self.df['data'] = pd.to_datetime(self.df['data'])
         self.monthly_df = None
         self.monthly_df_filled = False
         self.weekly_df = None
@@ -55,10 +57,12 @@ class CumStat:
         Helper function to create the monthly cumulated DataFrame.
         """
 
-        self.df['month'] = self.df['data'].apply(lambda x: x[:7].replace('-', '_'))
+        self.df['month'] = self.df['data'].apply(lambda x: x.strftime('%Y-%m'))
         self.monthly_df = self.df.groupby(['cod_art', 'month']).agg(cantitate=('cantitate', 'sum'),
                                                                     pret=('pret', 'mean'),
                                                                     valoare=('valoare', 'sum')).reset_index()
+        self.monthly_df = self.monthly_df.set_index('month')
+        self.monthly_df.index.name = 'data'
 
     def _create_weekly_df(self):
 
@@ -66,14 +70,13 @@ class CumStat:
         Helper function to create the weekly cumulated DataFrame.
         """
 
-        self.df['week_no'] = self.df['data'].apply(lambda x:
-                                                   datetime.date(*[int(s) for s in x.split('-')]).isocalendar())
-        self.df['week_no'] = self.df['week_no'].apply(lambda x: '{:d}_{:02d}'.format(x[0], x[1]))
+        self.df['week_no'] = self.df['data'].apply(lambda x: x.strftime('%Y-%U'))
         self.df = self.df.sort_values(by=['cod_art', 'week_no'])
 
         self.weekly_df = self.df.groupby(['cod_art', 'week_no']).agg(cantitate=('cantitate', 'sum'),
                                                                      pret=('pret', 'mean'),
                                                                      valoare=('valoare', 'sum')).reset_index()
+        self.weekly_df = self.weekly_df.set_index('week_no')
 
     def cumulate_monthly_all_articles(self, fill_missing_dates=False):
 
@@ -157,6 +160,7 @@ class CumStat:
 
         return self.weekly_df.copy()
 
+    # TODO: Adapt cumulative algorithms for date indexes as well
     def cumulate_monthly_article(self, cod_art):
 
         """
@@ -171,25 +175,25 @@ class CumStat:
         if self.monthly_df_filled:
             return self.monthly_df.query('cod_art == @cod_art')
 
-        cod_art_monthly_df = self.monthly_df.query('cod_art == @cod_art').sort_values(by='month')
+        cod_art_monthly_df = self.monthly_df.query('cod_art == @cod_art')
         start_end_date_df = self.df.groupby('cod_art').agg(start_date=('data', 'min'), end_date=('data', 'max'))
 
-        month_no_list = set()
-        for i in start_end_date_df.loc[cod_art]:
-            month_no = i[:7].replace('-', '_')
-            month_no_list.add(month_no)
+        present_months = {i: True for i in cod_art_monthly_df.index}
 
-        date_range = pd.date_range(*start_end_date_df.loc[cod_art].array, freq='M')
-        for i in date_range:
-            month_no = i.date().isoformat()[:7].replace('-', '_')
-            month_no_list.add(month_no)
+        date_range = pd.period_range(*start_end_date_df.loc[cod_art].array, freq='M')
 
-        for month_no in month_no_list:
-            contains = (cod_art_monthly_df['month'].eq(month_no)).any()
-            if not contains:
-                cod_art_monthly_df.loc[len(cod_art_monthly_df.index)] = [cod_art, month_no, 0, 0, 0]
+        filled_df = pd.DataFrame.from_dict({'cod_art': [cod_art for _ in range(len(date_range))],
+                                            'cantitate': [0 for _ in range(len(date_range))],
+                                            'pret': [0 for _ in range(len(date_range))],
+                                            'valoare': [0 for _ in range(len(date_range))]})
+        filled_df = filled_df.set_index(date_range)
+        print(filled_df)
 
-        return cod_art_monthly_df.sort_values(by='month')
+        for month_no in present_months:
+            filled_df.loc[month_no] = cod_art_monthly_df.loc[month_no, ['cod_art', 'cantitate', 'pret', 'valoare']]
+        filled_df['data'] = date_range.to_timestamp()
+
+        return filled_df
 
     def cumulate_weekly_article(self, cod_art):
 
@@ -208,31 +212,33 @@ class CumStat:
         cod_art_weekly_df = self.weekly_df.query('cod_art == @cod_art').sort_values(by='week_no')
         start_end_date_df = self.df.groupby('cod_art').agg(start_date=('data', 'min'), end_date=('data', 'max'))
 
-        week_no_list = set()
-        for i in start_end_date_df.loc[cod_art]:
-            week_no = datetime.date(*[int(s) for s in i.split('-')]).isocalendar()
-            week_no = '{:d}_{:02d}'.format(week_no[0], week_no[1])
-            week_no_list.add(week_no)
+        present_weeks = {i: True for i in cod_art_weekly_df.index}
 
-        date_range = pd.date_range(*start_end_date_df.loc[cod_art].array, freq='W')
-        for i in date_range:
-            week_no = i.date().isocalendar()
-            week_no = '{:d}_{:02d}'.format(week_no[0], week_no[1])
-            week_no_list.add(week_no)
+        date_range = pd.period_range(*start_end_date_df.loc[cod_art].array, freq='W')
+        date_range = pd.to_datetime([i.start_time for i in date_range])
+        date_range_index = date_range.strftime('%Y-%W')
 
-        for week_no in week_no_list:
-            contains = (cod_art_weekly_df['week_no'].eq(week_no)).any()
-            if not contains:
-                cod_art_weekly_df.loc[len(cod_art_weekly_df.index)] = [cod_art, week_no, 0, 0, 0]
+        filled_df = pd.DataFrame.from_dict({'cod_art': [cod_art for _ in range(len(date_range))],
+                                            'cantitate': [0 for _ in range(len(date_range))],
+                                            'pret': [0 for _ in range(len(date_range))],
+                                            'valoare': [0 for _ in range(len(date_range))]})
 
-        return cod_art_weekly_df.sort_values(by='week_no')
+        filled_df = filled_df.set_index(date_range_index)
+        print(filled_df)
+
+        for week_no in present_weeks:
+            filled_df.loc[week_no] = cod_art_weekly_df.loc[week_no, ['cod_art', 'cantitate', 'pret', 'valoare']]
+
+        filled_df['data'] = date_range
+
+        return filled_df
 
     def plot_monthly(self, cod_art, filename=None):
         monthly_df = self.cumulate_monthly_article(cod_art)
-        cod_art_df = monthly_df.query(f'cod_art == {cod_art}')
-        cod_art_df.plot(x='month', y='cantitate')
+        plt.plot(range(len(monthly_df)), monthly_df['cantitate'], label='cantitate')
+        plt.legend()
 
-        plt.xticks(range(len(cod_art_df['month'])), cod_art_df['month'], rotation=45)
+        plt.xticks(range(len(monthly_df)), monthly_df.index, rotation=45)
         plt.ylabel('Cantitate')
         plt.tight_layout()
         if filename:
@@ -242,11 +248,10 @@ class CumStat:
 
     def plot_weekly(self, cod_art, filename=None):
         weekly_df = self.cumulate_weekly_article(cod_art)
-        cod_art_df = weekly_df.query(f'cod_art == {cod_art}')
-        cod_art_df.plot(x='week_no', y='cantitate')
+        weekly_df.plot(y='cantitate', use_index=True)
 
-        plt.xticks(range(len(cod_art_df['week_no'])), cod_art_df['week_no'], rotation=60)
-        plt.xticks(fontsize=5)
+        plt.xticks(rotation=60)
+        plt.xticks(fontsize=10)
         plt.ylabel('Cantitate')
         plt.tight_layout()
         if filename:
@@ -316,37 +321,37 @@ if __name__ == '__main__':
 
     monthly_stat, weekly_stat = None, None
 
-    # cumstat.plot_monthly(74, 'monthly_74.png')
-    # cumstat.plot_weekly(74, 'weekly_74.png')
+    cumstat.plot_monthly(74, 'monthly_74.png')
+    cumstat.plot_weekly(74, 'weekly_74.png')
 
-    if monthly_df:
-        monthly_stat = cumstat.cumulate_monthly_all_articles(fill_missing_dates=False)
-        monthly_stat.to_csv(monthly_df, index=False)
-
-    if monthly_stat_out_path:
-        monthly_stat = cumstat.cumulate_monthly_all_articles() if monthly_stat is None else monthly_stat
-
-        monthly_cumstat = CumStat(df=monthly_stat)
-        monthly_cumstat.add_aggregator('cantitate', 'min')
-        monthly_cumstat.add_aggregator('cantitate', 'max')
-        monthly_cumstat.add_aggregator('cantitate', 'mean')
-        monthly_cumstat.add_aggregator('cantitate', 'std')
-
-        monthly_agg = monthly_cumstat.group_by('cod_art')
-        monthly_agg.to_csv(monthly_stat_out_path, index=False)
-
-    if weekly_df:
-        weekly_stat = cumstat.cumulate_weekly_all_articles(fill_missing_dates=False)
-        weekly_stat.to_csv(weekly_df, index=False)
-
-    if weekly_stat_out_path:
-        weekly_stat = cumstat.cumulate_weekly_all_articles() if weekly_stat is None else weekly_stat
-
-        weekly_cumstat = CumStat(df=weekly_stat)
-        weekly_cumstat.add_aggregator('cantitate', 'min')
-        weekly_cumstat.add_aggregator('cantitate', 'max')
-        weekly_cumstat.add_aggregator('cantitate', 'mean')
-        weekly_cumstat.add_aggregator('cantitate', 'std')
-
-        weekly_agg = weekly_cumstat.group_by('cod_art').reset_index()
-        weekly_agg.to_csv(weekly_stat_out_path, index=False)
+    # if monthly_df:
+    #     monthly_stat = cumstat.cumulate_monthly_all_articles(fill_missing_dates=False)
+    #     monthly_stat.to_csv(monthly_df, index=False)
+    #
+    # if monthly_stat_out_path:
+    #     monthly_stat = cumstat.cumulate_monthly_all_articles() if monthly_stat is None else monthly_stat
+    #
+    #     monthly_cumstat = CumStat(df=monthly_stat)
+    #     monthly_cumstat.add_aggregator('cantitate', 'min')
+    #     monthly_cumstat.add_aggregator('cantitate', 'max')
+    #     monthly_cumstat.add_aggregator('cantitate', 'mean')
+    #     monthly_cumstat.add_aggregator('cantitate', 'std')
+    #
+    #     monthly_agg = monthly_cumstat.group_by('cod_art')
+    #     monthly_agg.to_csv(monthly_stat_out_path, index=False)
+    #
+    # if weekly_df:
+    #     weekly_stat = cumstat.cumulate_weekly_all_articles(fill_missing_dates=False)
+    #     weekly_stat.to_csv(weekly_df, index=False)
+    #
+    # if weekly_stat_out_path:
+    #     weekly_stat = cumstat.cumulate_weekly_all_articles() if weekly_stat is None else weekly_stat
+    #
+    #     weekly_cumstat = CumStat(df=weekly_stat)
+    #     weekly_cumstat.add_aggregator('cantitate', 'min')
+    #     weekly_cumstat.add_aggregator('cantitate', 'max')
+    #     weekly_cumstat.add_aggregator('cantitate', 'mean')
+    #     weekly_cumstat.add_aggregator('cantitate', 'std')
+    #
+    #     weekly_agg = weekly_cumstat.group_by('cod_art').reset_index()
+    #     weekly_agg.to_csv(weekly_stat_out_path, index=False)
