c7de3e55ab97572bf9eeb3dbc4f678b6ab31b4fe
Balog Szilard (BEG/ENS1-RO) <Szilard.Balog@ro.bosch.com>
2023-06-19 17:02:04 +0300

    Unifying the visualization and evaluation of the predictions for the different forecast method and adding comparison to the mean forecast.

--- forecast/prophet/prophet_forecast.py
+++ forecast/prophet/prophet_forecast.py
@@ -4,6 +4,7 @@ from data.data_generator import DataLoader
 from matplotlib import pyplot as plt
 from sklearn.metrics import mean_squared_error
 from utils.path_handling import handle_parent_path
+import numpy as np
 
 
 def predict_all_cod_arts(frequency='daily', column='valoare'):
@@ -75,7 +76,7 @@ def predict_all_categories(frequency='daily', column='valoare'):
                                           end_date=pd.to_datetime('2023-01-01'))
 
         m = Prophet(interval_width=0.80, daily_seasonality=False, weekly_seasonality=True, yearly_seasonality=True)
-        if len(train) + len(val) >= 50:
+        if len(train) + len(val) >= 100:
             m.fit(train)
 
             future = m.make_future_dataframe(periods=100)
@@ -90,42 +91,44 @@ def predict_all_categories(frequency='daily', column='valoare'):
             forecast = m.predict(future)
             forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-            fig1 = m.plot(forecast)
-
-            ax = plt.gca()
-            ax.scatter(val['ds'], val['y'], c='red', s=10)
+            # fig1 = m.plot(forecast)
+            #
+            # ax = plt.gca()
+            # ax.scatter(val['ds'], val['y'], c='red', s=10)
 
             # plt.show()
             filename = f'charts/{frequency}/{column}/categories/{category}.png'
             handle_parent_path(filename)
-            plt.savefig(filename, dpi=300)
+            plot_prophet_forecast(pd.concat([train, val]), val_forecast, frequency, column, filename)
+            # plt.savefig(filename, dpi=300)
 
             # fig2 = m.plot_components(forecast)
             # plt.show()
 
     df_dict = {'category': list(mse_losses.keys()), 'mse_loss': mse_losses.values()}
     loss_df = pd.DataFrame.from_dict(df_dict)
-    filename = f'charts/{frequency}/{column}/prophet_categories_losses.csv'
+    filename = f'charts/{frequency}/{column}/prophet_category_losses.csv'
     handle_parent_path(filename)
     loss_df.to_csv(filename, index=False)
 
 
-def plot_forecast(y_true, y_pred, frequency, column):
+def plot_prophet_forecast(y_data, y_pred, frequency, column, filename):
     fig, ax = plt.subplots()
 
-    ax.plot(y_true['ds'], y_true['y'])
-    ax.plot(y_pred['ds'], y_pred['yhat'], 'r')
+    fig.set_size_inches((7, 3))
 
-    ax.plot(y_pred['ds'], y_pred['yhat_lower'], c='red', alpha=0.5, linewidth=1)
-    ax.plot(y_pred['ds'], y_pred['yhat_upper'], c='red', alpha=0.5, linewidth=1)
+    ax.plot(y_data['ds'], y_data['y'], linewidth=1)
+    ax.plot(y_pred['ds'], y_pred['yhat'], 'r', linewidth=1)
+
+    ax.plot(y_pred['ds'], y_pred['yhat_lower'], c='red', alpha=0.5, linewidth=0.5)
+    ax.plot(y_pred['ds'], y_pred['yhat_upper'], c='red', alpha=0.5, linewidth=0.5)
     ax.fill_between(y_pred['ds'], y_pred['yhat_lower'], y_pred['yhat_upper'], color='red', alpha=0.1)
 
-    filename = f'charts/{frequency}/{column}/prophet_forecast_combined_val.png'
     handle_parent_path(filename)
     plt.savefig(filename, dpi=300)
 
 
-def predicts_combined_products(frequency='daily', column='valoare'):
+def predict_combined_products(frequency='daily', column='valoare'):
     dataloader = DataLoader(path_to_csv=r'../../data/combined.csv')
 
     # train, val = dataloader.load_combined_data(column,
@@ -137,7 +140,7 @@ def predicts_combined_products(frequency='daily', column='valoare'):
                                       start_date=pd.to_datetime('2022-01-01'),
                                       end_date=pd.to_datetime('2023-01-01'))
 
-    m = Prophet(interval_width=0.9, daily_seasonality=True, weekly_seasonality=True, yearly_seasonality=True)
+    m = Prophet(interval_width=0.9, daily_seasonality=False, weekly_seasonality=True, yearly_seasonality=True)
     if len(train) + len(val) >= 50:
         m.fit(train)
 
@@ -153,16 +156,18 @@ def predicts_combined_products(frequency='daily', column='valoare'):
         loss = mean_squared_error(val['y'], val_forecast['yhat'])
         print(loss)
 
-        fig1 = m.plot(forecast)
+        # fig1 = m.plot(forecast)
 
-        ax = plt.gca()
-        ax.scatter(val['ds'], val['y'], c='red', s=10)
+
+
+        # ax = plt.gca()
+        # ax.scatter(val['ds'], val['y'], c='red', s=10)
         #
         # plt.show()
 
-        filename = fr'charts/{frequency}/{column}/prophet_111.png'
-        handle_parent_path(filename)
-        plt.savefig(filename, dpi=300)
+        # filename = fr'charts/{frequency}/{column}/prophet_011.png'
+        # handle_parent_path(filename)
+        # plt.savefig(filename, dpi=300)
         #
         # fig2 = m.plot_components(forecast)
         # plt.show()
@@ -172,10 +177,65 @@ def predicts_combined_products(frequency='daily', column='valoare'):
         handle_parent_path(filename)
         loss_df.to_csv(filename, index=False)
 
-        plot_forecast(val, val_forecast, frequency, column)
+        plot_filename = f'charts/{frequency}/{column}/prophet_forecast_combined_val.png'
+        plot_prophet_forecast(val, val_forecast, frequency, column, plot_filename)
+        plot_filename = f'charts/{frequency}/{column}/prophet_forecast_combined.png'
+        plot_prophet_forecast(pd.concat([train, val]), val_forecast, frequency, column, plot_filename)
+
+
+def compare_to_mean(item_type=None, frequency='daily', column='valoare'):
+    dataloader = DataLoader(path_to_csv=r'../../data/combined.csv')
+
+    mse_loss = []
+    prophet_losses = []
+    diffs = []
+    relevant_type_identifiers = []
+
+    type_identifiers = [None]
+    if item_type == 'category':
+        type_identifiers = dataloader.get_categories()
+    elif item_type == 'cod_art':
+        type_identifiers = dataloader.get_article_ids()
+
+    df = pd.read_csv(f'charts/{frequency}/{column}/prophet_{str(item_type)}_losses.csv', index_col=str(item_type))
+
+    for type_identifier in type_identifiers:
+
+        train, val = dataloader.load_data(frequency=frequency,
+                                          value_type=column,
+                                          item_type=item_type,
+                                          type_identifier=type_identifier,
+                                          start_date=pd.to_datetime('2022-01-01'),
+                                          end_date=pd.to_datetime('2023-01-01'))
+
+        if len(train) + len(val) > 100:
+            relevant_type_identifiers.append(type_identifier)
+
+            mean = train['y'].mean()
+            mean_val = pd.DataFrame.from_dict({'ds': val['ds'], 'y': mean * np.ones(len(val))})
+
+            loss = mean_squared_error(val['y'], mean_val['y'])
+            prophet_loss = df.loc[type_identifier, 'mse_loss']
+            diff = prophet_loss - loss
+
+            mse_loss.append(loss)
+            prophet_losses.append(prophet_loss)
+            diffs.append(diff)
+
+    df = pd.DataFrame.from_dict({
+        str(item_type): relevant_type_identifiers,
+        'mse_loss': mse_loss,
+        'prophet_loss': prophet_losses,
+        'diff': diffs})
+    filename = f'charts/comparison_to_mean/{frequency}/{column}/prophet_category_compare_to_mean.csv'
+    handle_parent_path(filename)
+    df.to_csv(filename, index=False)
+
+
 
 
 if __name__ == '__main__':
-    predict_all_categories(frequency='daily', column='valoare')
+    # predict_all_categories(frequency='daily', column='valoare')
     # predict_all_cod_arts(frequency='daily', column='valoare')
-    # predicts_combined_products(frequency='weekly', column='valoare')
+    # predict_combined_products(frequency='weekly', column='valoare')
+    compare_to_mean(item_type='category', frequency='daily', column='valoare')
--- forecast/statsmodels/statsmodels_forecast.py
+++ forecast/statsmodels/statsmodels_forecast.py
@@ -3,9 +3,11 @@ import statsmodels
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import warnings
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.arima_model import ARMAResults
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
 from data.data_generator import DataLoader
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
@@ -13,6 +15,9 @@ from datetime import timedelta
 from sklearn.metrics import mean_squared_error
 from utils.path_handling import handle_parent_path
 from tqdm import tqdm
+
+warnings.simplefilter('ignore', ConvergenceWarning)
+warnings.simplefilter('ignore', UserWarning)
 
 
 def check_stationarity(ts):
@@ -63,19 +68,18 @@ def plot_autocorrelation(frequency, column='valoare'):
     plt.show()
 
 
-def plot_forecast(y_true, y_pred, frequency, column, p=None, d=None, q=None, m=None, draw_plot=False):
+def plot_forecast(y_true, y_pred, filename, draw_plot=False):
     fig, ax = plt.subplots()
 
-    ax.plot(y_true['ds'], y_true['y'])
-    ax.plot(y_pred['ds'], y_pred['y'], 'r')
+    ax.plot(y_true['ds'], y_true['y'], linewidth=1)
+    ax.plot(y_pred['ds'], y_pred['y'], 'r', linewidth=1)
 
-    ax.plot(y_pred['ds'], y_pred['lower y'], c='red', alpha=0.5, linewidth=1)
-    ax.plot(y_pred['ds'], y_pred['upper y'], c='red', alpha=0.5, linewidth=1)
+    ax.plot(y_pred['ds'], y_pred['lower y'], c='red', alpha=0.5, linewidth=0.5)
+    ax.plot(y_pred['ds'], y_pred['upper y'], c='red', alpha=0.5, linewidth=0.5)
     ax.fill_between(y_pred['ds'], y_pred['lower y'], y_pred['upper y'], color='red', alpha=0.1)
 
-    val_plot_filename = f'charts/{frequency}/{column}/statsmodels_combined_{p}_{d}_{q}_{m}_val.png'
-    handle_parent_path(val_plot_filename)
-    plt.savefig(val_plot_filename, dpi=300)
+    handle_parent_path(filename)
+    plt.savefig(filename, dpi=300)
 
     if draw_plot:
         plt.show()
@@ -116,25 +120,17 @@ def predict_combined_products(frequency='daily', column='valoare'):
     loss = mean_squared_error(val['y'], y_pred_df['y'])
     print('loss = ', loss)
 
-    loss_df = pd.DataFrame.from_dict({'name': [column], 'loss': [loss], 'adjusted_loss': [loss * 0.000001 * ARMAmodel.bic * ARMAmodel.aic]})
+    loss_df = pd.DataFrame.from_dict(
+        {'name': [column], 'loss': [loss], 'adjusted_loss': [loss * 0.000001 * ARMAmodel.bic * ARMAmodel.aic]})
     loss_df_filename = f'charts/{frequency}/{column}/statsmodels_combined_{p}_{d}_{q}_{m}.csv'
     handle_parent_path(loss_df_filename)
     loss_df.to_csv(loss_df_filename, index=False)
 
-    fig.set_size_inches(8, 5)
-    ax.plot(y_pred_df['ds'], y_pred_df['y'], c='red')
-
-    # plot uncertainty
-    ax.plot(y_pred_df['ds'], y_pred_df['lower y'], c='red', alpha=0.5, linewidth=1)
-    ax.plot(y_pred_df['ds'], y_pred_df['upper y'], c='red', alpha=0.5, linewidth=1)
-    ax.fill_between(y_pred_df['ds'], y_pred_df['lower y'], y_pred_df['upper y'], color='red', alpha=0.1)
-
     train_val_plot_filename = f'charts/{frequency}/{column}/statsmodels_{p}_{d}_{q}_{m}.png'
-    handle_parent_path(train_val_plot_filename)
-    plt.savefig(train_val_plot_filename, dpi=300)
-    plt.show()
+    plot_forecast(pd.concat([train, val]), y_pred_df, train_val_plot_filename)
 
-    plot_forecast(val, y_pred_df, frequency=frequency, p=p, d=d, q=q, m=m, column=column)
+    val_plot_filename = f'charts/{frequency}/{column}/statsmodels_combined_{p}_{d}_{q}_{m}_val.png'
+    plot_forecast(val, y_pred_df, val_plot_filename)
 
 
 def grid_search(train, val, type_identifier, frequency='daily', column='valoare'):
@@ -165,9 +161,6 @@ def grid_search(train, val, type_identifier, frequency='daily', column='valoare'
     for p in tqdm(range(1, max_p + 1), desc='p'):
         for q in tqdm(range(1, max_q + 1), desc='q'):
             for d in tqdm(range(0, max_d + 1), desc='d'):
-
-                # print('Testing for: ', p, d, q)
-
                 try:
                     ARMAmodel = SARIMAX(train['y'], order=(p, d, q))
                     ARMAmodel = ARMAmodel.fit(disp=0)
@@ -176,7 +169,7 @@ def grid_search(train, val, type_identifier, frequency='daily', column='valoare'
 
                 y_pred = ARMAmodel.get_forecast(len(val['ds']))
                 y_pred_df = y_pred.conf_int(alpha=0.05)
-                y_pred_df["y"] = ARMAmodel.predict(start=y_pred_df.index[0], end=y_pred_df.index[-1])
+                y_pred_df['y'] = ARMAmodel.predict(start=y_pred_df.index[0], end=y_pred_df.index[-1])
                 y_pred_df['ds'] = val['ds']
                 y_pred_df.index = val.index
 
@@ -202,22 +195,23 @@ def grid_search(train, val, type_identifier, frequency='daily', column='valoare'
                 if ARMAmodel.aic < best_non_seasonal['aic']:
                     best_non_seasonal['aic'] = ARMAmodel.aic
 
-                fig, ax = plt.subplots()
-                fig.set_size_inches(8, 5)
-
-                ax.plot(val['ds'], val['y'])
-                ax.plot(y_pred_df['ds'], y_pred_df['y'], c='red')
-
-                # plot uncertainty
-                ax.plot(y_pred_df['ds'], y_pred_df['lower y'], c='red', alpha=0.5, linewidth=1)
-                ax.plot(y_pred_df['ds'], y_pred_df['upper y'], c='red', alpha=0.5, linewidth=1)
-                ax.fill_between(y_pred_df['ds'], y_pred_df['lower y'], y_pred_df['upper y'], color='red', alpha=0.1)
-
-                train_val_plot_filename = f'charts/grid_search/{frequency}/{column}/{type_identifier}/non_seasonal/statsmodels_p={p}_d={d}_q={q}_loss={loss:.4f}.png'
-                handle_parent_path(train_val_plot_filename)
-                plt.savefig(train_val_plot_filename, dpi=300)
+                val_plot_filename = f'charts/grid_search/{frequency}/{column}/{type_identifier}/non_seasonal/statsmodels_p={p}_d={d}_q={q}_loss={loss:.4f}.png'
+                plot_forecast(val, y_pred_df, val_plot_filename)
 
     print('Best non-seasonal values: ', best_non_seasonal)
+    ARMAmodel = SARIMAX(train['y'], order=(best_non_seasonal['p'], best_non_seasonal['d'], best_non_seasonal['q']))
+    ARMAmodel = ARMAmodel.fit(disp=0)
+
+    y_pred = ARMAmodel.get_forecast(len(val['ds']))
+    y_pred_df = y_pred.conf_int(alpha=0.05)
+    y_pred_df['y'] = ARMAmodel.predict(start=y_pred_df.index[0], end=y_pred_df.index[-1])
+    y_pred_df['ds'] = val['ds']
+    loss = mean_squared_error(val['y'], y_pred_df['y'])
+
+    p, d, q, loss = best_non_seasonal['p'], best_non_seasonal['d'], best_non_seasonal['q'], best_non_seasonal['loss']
+
+    train_val_plot_filename = f'charts/grid_search/{frequency}/{column}/{type_identifier}/non_seasonal/best_statsmodels_p={p}_d={d}_q={q}_loss={loss:.4f}.png'
+    plot_forecast(pd.concat([train, val]), y_pred_df, train_val_plot_filename)
 
     non_seasonal_df.to_csv(non_seasonal_df_filename)
 
@@ -301,16 +295,71 @@ def grid_search(train, val, type_identifier, frequency='daily', column='valoare'
     # print('Best seasonal values: ', best_non_seasonal)
     # seasonal_df.to_csv(seasonal_df_filename)
 
+    return loss, p, d, q
+
 
 def grid_search_item_types(item_type=None, frequency='daily', column='valoare'):
-
     dataloader = DataLoader(path_to_csv=r'../../data/combined.csv')
+
+    mse_loss = []
+    p_values = []
+    d_values = []
+    q_values = []
+
+    relevant_type_identifiers = []
 
     type_identifiers = [None]
     if item_type == 'category':
         type_identifiers = dataloader.get_categories()
     elif item_type == 'cod_art':
         type_identifiers = dataloader.get_article_ids()
+
+    for type_identifier in type_identifiers:
+        print(type_identifier, ':')
+
+        train, val = dataloader.load_data(frequency=frequency,
+                                          value_type=column,
+                                          item_type=item_type,
+                                          type_identifier=type_identifier,
+                                          start_date=pd.to_datetime('2022-01-01'),
+                                          end_date=pd.to_datetime('2023-01-01'))
+
+        if len(train) + len(val) > 100:
+            relevant_type_identifiers.append(type_identifier)
+
+            print(type_identifier)
+            loss, p, d, q = grid_search(train, val, type_identifier, frequency, column)
+            mse_loss.append(loss)
+            p_values.append(p)
+            d_values.append(d)
+            q_values.append(q)
+
+    df = pd.DataFrame.from_dict({
+        str(item_type): relevant_type_identifiers,
+        'mse_loss': mse_loss,
+        'p': p_values,
+        'd': d_values,
+        'q': q_values})
+    filename = f'charts/grid_search/{frequency}/{column}/statsmodels_{item_type}_grid_search.csv'
+    handle_parent_path(filename)
+    df.to_csv(filename, index=True)
+
+
+def compare_to_mean(item_type=None, frequency='daily', column='valoare'):
+    dataloader = DataLoader(path_to_csv=r'../../data/combined.csv')
+
+    mse_loss = []
+    statsmodels_losses = []
+    diffs = []
+    relevant_type_identifiers = []
+
+    type_identifiers = [None]
+    if item_type == 'category':
+        type_identifiers = dataloader.get_categories()
+    elif item_type == 'cod_art':
+        type_identifiers = dataloader.get_article_ids()
+
+    df = pd.read_csv(f'charts/grid_search/{frequency}/{column}/statsmodels_{item_type}_grid_search.csv', index_col=str(item_type))
 
     for type_identifier in type_identifiers:
 
@@ -321,15 +370,37 @@ def grid_search_item_types(item_type=None, frequency='daily', column='valoare'):
                                           start_date=pd.to_datetime('2022-01-01'),
                                           end_date=pd.to_datetime('2023-01-01'))
 
-        if len(train) > 25:
-            grid_search(train, val, type_identifier, frequency, column)
+        if len(train) + len(val) > 100:
+            relevant_type_identifiers.append(type_identifier)
+
+            mean = train['y'].mean()
+            mean_val = pd.DataFrame.from_dict({'ds': val['ds'], 'y': mean * np.ones(len(val))})
+
+            loss = mean_squared_error(val['y'], mean_val['y'])
+            statsmodels_loss = df.loc[type_identifier, 'mse_loss']
+            diff = statsmodels_loss - loss
+
+            mse_loss.append(loss)
+            statsmodels_losses.append(statsmodels_loss)
+            diffs.append(diff)
+
+    df = pd.DataFrame.from_dict({
+        str(item_type): relevant_type_identifiers,
+        'mse_loss': mse_loss,
+        'statsmodels_loss': statsmodels_losses,
+        'diff': diffs})
+    filename = f'charts/comparison_to_mean/{frequency}/{column}/statsmodels_{item_type}_compare_to_mean.csv'
+    handle_parent_path(filename)
+    df.to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
-    frequency = 'weekly'
+    frequency = 'daily'
     column = 'valoare'
 
     # plot_autocorrelation(frequency=frequency, column=column)
     # predict_combined_products(frequency=frequency, column=column)
 
-    grid_search_item_types(item_type='category', frequency=frequency, column=column)
+    # grid_search_item_types(item_type='category', frequency=frequency, column=column)
+
+    compare_to_mean(item_type='category', frequency=frequency, column=column)
