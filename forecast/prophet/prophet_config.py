e676aaf4948a4b82d233153c8bf61ddc4cb91b76
Balog Szilard (BEG/ENS1-RO) <Szilard.Balog@ro.bosch.com>
2023-08-01 08:45:19 +0300

    Adding combined csv path to the config.

--- forecast/prophet/prophet_config.py
+++ forecast/prophet/prophet_config.py
@@ -7,6 +7,9 @@ class ProphetConfig(Config):
     def __init__(self, yaml_path):
         super().__init__(yaml_path)
 
+        self.combined_csv_path = self.yaml_obj['combined_csv_path']
+        if self.combined_csv_path is None :
+            self.combined_csv_path = fr'{self.project_root}/data/combined.csv'
         self.frequency = self.yaml_obj['frequency']
         self.group_identifiers = self.yaml_obj['group_identifiers']
         self.smoothing_window_size = self.yaml_obj['smoothing_window_size']
@@ -19,4 +22,4 @@ class ProphetConfig(Config):
         self.start_date = pd.to_datetime(self.yaml_obj['start_date'])
         self.end_date = pd.to_datetime(self.yaml_obj['end_date'])
         self.group_by_col = 'category' if 'category' in self.group_identifiers else 'cod_art'
-        self.dataloader = DataLoader(path_to_csv=fr'{self.project_root}/data/combined.csv')
+        self.dataloader = DataLoader(path_to_csv=self.combined_csv_path)
